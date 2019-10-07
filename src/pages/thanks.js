3a2bfe47db7f7386be1f7b507f25149ac42b0bf1
Corinn Pope <corinn.pope@gmail.com>
2019-10-07 10:51:36 -0500

    add a thank you page

---
+++ src/pages/thanks.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default () => (
+  <Layout>
+    <h1>Thank you! 🙏 </h1>
+    <p>I'll keep you updated as we move towards launch! </p>
+    <p>In the meantime we're going deep into the code cave, ruthlessly prioritizing, 
+    and building the product I wish I had as a product director.</p>
+    <p>💛 Corinn</p>
+    <p>p.s follow along on the journey: <a href="https://twitter.com/getspeckled?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-show-count="false">@getspeckled</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </p>
+
+  </Layout>
+)
