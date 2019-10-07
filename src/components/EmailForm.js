884cce7cf619499f331c87c0ce1c1971672e4544
Corinn Pope <corinn.pope@gmail.com>
2019-10-06 20:30:24 -0500

    whoops forgot a field

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,9 +1,11 @@
-<form name="contact" method="POST" data-netlify="true">
+<form name="contact" id="signup-form" method="POST" data-netlify="true">
   <p>
     <label>Your Name: <input type="text" name="name" /></label>   
+    <input type="text" name="name" id="name" />
   </p>
   <p>
     <label>Your Email: <input type="email" name="email" /></label>
+    <input type="text" name="email" id="email" />
   </p>
   <p>
     <button type="submit">Get Updates</button>
