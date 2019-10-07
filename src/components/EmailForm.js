4ced6befaf903a3ed95c09ef126781fa6b3db64b
Corinn Pope <corinn.pope@gmail.com>
2019-10-06 20:40:08 -0500

    I don't think i like react - another stupid mistake

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -7,14 +7,13 @@ export class EmailForm extends Component {
       <form name="contact" id="signup-form" method="POST" data-netlify="true">
   <p>
     <label>Your Name: <input type="text" name="name" /></label>   
-    <input type="text" name="name" id="name" />
   </p>
   <p>
     <label>Your Email: <input type="email" name="email" /></label>
 
   </p>
   <p>
-    <button type="submit">Get Updates</button>
+    <input type="submit">Get Updates</button>
   </p>
 </form>
     );
