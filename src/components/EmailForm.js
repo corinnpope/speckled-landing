06712e939d90b06ebd7841cc93bd42809ef8c980
Corinn Pope <corinn.pope@gmail.com>
2019-10-06 20:35:40 -0500

    I don't think i like react

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 
-<form name="contact" id="signup-form" method="POST" data-netlify="true">
+export class EmailForm extends Component {
+  render() {
+    
+    return (
+      <form name="contact" id="signup-form" method="POST" data-netlify="true">
   <p>
     <label>Your Name: <input type="text" name="name" /></label>   
     <input type="text" name="name" id="name" />
   </p>
   <p>
     <label>Your Email: <input type="email" name="email" /></label>
-    <input type="text" name="email" id="email" />
+
   </p>
   <p>
     <button type="submit">Get Updates</button>
   </p>
 </form>
+    );
+  }
+}
+
+export default EmailForm;
+
+
