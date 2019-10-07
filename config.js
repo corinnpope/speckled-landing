d99d859f3b24140f68c0c33353b51ce557888700
Corinn Pope <corinn.pope@gmail.com>
2019-10-06 20:19:17 -0500

    customizations

--- config.js
+++ config.js
@@ -1,37 +1,46 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Eventually', // <title>
-  manifestName: 'Eventually',
+  siteTitle: 'Speckled - Making better product decisions', // <title>
+  manifestName: 'Speckled',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
-  manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663399',
+  manifestBackgroundColor: '#F8B500',
+  manifestThemeColor: '#F8B500',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
+   // pathPrefix: `/gatsby-starter-eventually/`, This path is subpath of your hosting https://domain/portfolio
   // social
-  heading: 'Eventually',
+  heading: 'Speckled',
   subHeading:
-    "A simple template for telling the world when you'll launch your next big thing.",
+    "Make better product decisions",
   socialLinks: [
-    {
-      icon: 'fa-github',
-      name: 'Github',
-      url: 'https://github.com/anubhavsrivastava',
-    },
+    // {
+    //   icon: 'fa-github',
+    //   name: 'Github',
+    //   url: 'https://github.com/anubhavsrivastava',
+    // },
     {
       icon: 'fa-twitter',
       name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
+      url: 'https://twitter.com/getspeckled',
     },
     {
       icon: 'fa-facebook',
       name: 'Facebook',
-      url: 'https://facebook.com/theanubhav',
+      url: 'https://facebook.com/getspeckled',
     },
     {
       icon: 'fa-envelope-o',
       name: 'Email',
-      url: 'mailto:anubhav.srivastava00@gmail.com',
+      url: 'mailto:hello@getspeckled.com',
     },
   ],
+plugins: [
+    {
+        resolve: 'gatsby-plugin-mailchimp',
+        options: {
+            endpoint: 'https://getspeckled.us20.list-manage.com/subscribe/post?u=0f9da1f175b82fa0601dea5c3&amp;id=ccd3356148', // add your MC list endpoint here; see instructions below
+        },
+    },
+],
 };
+
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -38,8 +38,8 @@ class Layout extends Component {
             <Helmet
               title={data.site.siteMetadata.title}
               meta={[
-                { name: 'description', content: 'Eventually' },
-                { name: 'keywords', content: 'site, web' },
+                { name: 'description', content: 'Speckled' },
+                { name: 'keywords', content: 'product management, software, prioritization software, roadmap software' },
               ]}
             >
               <html lang="en" />
