d2c279aad08379404034670c64a1f64a5e1d35b8
Corinn Pope <corinn.pope@gmail.com>
2019-10-06 20:26:39 -0500

    netlify form

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,38 +1,11 @@
-import React, { Component } from 'react';
-
-export class EmailForm extends Component {
-  constructor() {
-    super();
-    this.state = { message: '' };
-    this.onSubmit = this.onSubmit.bind(this);
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    this.setState({ message: 'Thank you!' });
-    setTimeout(() => {
-      this.setState({ message: '' });
-    }, 3000);
-  }
-
-  render() {
-    const { message } = this.state;
-    return (
-      <form id="signup-form" onSubmit={this.onSubmit} method="post" action="#">
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email Address"
-        />
-        <input type="submit" value="Sign Up" />
-        <span className={`${message ? 'visible success' : ''} message`}>
-          {message}
-        </span>
-      </form>
-    );
-  }
-}
-
-export default EmailForm;
+<form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Get Updates</button>
+  </p>
+</form>
